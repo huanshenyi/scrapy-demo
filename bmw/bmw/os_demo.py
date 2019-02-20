87612e8c1a80db01b29a7dc1bab9ae64dc6e423c
Syoitu Den <42715311+huanshenyi@users.noreply.github.com>
2019-02-20 18:02:50 +0900

    Add files via upload

---
+++ bmw/bmw/os_demo.py
@@ -0,0 +1,6 @@
+import os
+
+images_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'images')
+# print(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'images'))
+if not os.path.exists(images_path):
+    os.mkdir(images_path)
